2274312d683e6c14bc11d407e9dadf3a3b426390
nabd0018 <nabd0018@student.monash.edu>
2022-04-04 17:21:10 +0200

    commented functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,33 @@
+"""
+ALPINE INSTALL SCREEN Application
+- Records all submitted user records as a an array of json objects in the file "users.json"
+
+Created by: Nadeem Abdelkader on 3/4/2022
+Last updated by Nadeem Abdelkader on 4/4/2022
+
+GUI framework = Tkinter
+
+This file contains the helper function to be called from main.py
+"""
+
+# importing the necessary libraries for working with csv, Tkinter and MySQL
 import json
 import os
 from tkinter import Frame, Label, Entry, X, LEFT, RIGHT, YES, messagebox, Button, Tk, TOP
 
-fields = ('User Name', 'Group Name', 'Active Directory Name', 'Password', 'Re-enter Password', 'Host Name',
+# declaring the constants to be used everywhere in the module
+FIELDS = ('User Name', 'Group Name', 'Active Directory Name', 'Password', 'Re-enter Password', 'Host Name',
           'Interface Name', 'IP address', 'Network Name', 'Gateway', 'DNS')
-users_file_name = "users.json"
+USERS_FILENAME = "users.json"
 
 
 def make_form(root, fields):
+    """
+    This function created the actual GUI form using Tkinter Entry, Label, and Frames
+    :param root: root Tkinter window
+    :param fields: array of strings that include the field names to createb the form according to
+    :return: an array of Tkinter entries
+    """
     makeLabel(root)
     entries = {}
     for field in fields:
@@ -26,25 +46,37 @@ def make_form(root, fields):
 
 
 def makeLabel(root):
+    """
+    This function adds the GUI heading
+    :param root: root Tkinter window
+    :return: void
+    """
     txt_title = Label(root, width=0, font=(
         'arial', 24), text="Khwarizm Consulting")
     txt_title.pack(side=TOP, padx=5, pady=5)
+    return
 
 
 def read_from_json(filename):
+    """
+    This function reads the previous users data from a .json file that can contain 0 or more json objects
+    (stored as an array of json objects)
+    :param filename: file to get the records from
+    :return: an array of json objects
+    """
     input_file = open(filename)
     json_array = json.load(input_file)
     user_list = []
 
     for user in json_array:
-        user_details = {fields[0]: None, fields[1]: None, fields[2]: None, fields[3]: None,
-                        fields[4]: None, fields[5]: None, fields[6]: None, fields[7]: None,
-                        fields[8]: None, fields[9]: None, fields[10]: None, fields[0]: user[fields[0]],
-                        fields[1]: user[fields[1]], fields[2]: user[fields[2]],
-                        fields[3]: user[fields[3]], fields[4]: user[fields[4]],
-                        fields[5]: user[fields[5]], fields[6]: user[fields[6]],
-                        fields[7]: user[fields[7]], fields[8]: user[fields[8]],
-                        fields[9]: user[fields[9]], fields[10]: user[fields[10]]}
+        user_details = {FIELDS[0]: None, FIELDS[1]: None, FIELDS[2]: None, FIELDS[3]: None,
+                        FIELDS[4]: None, FIELDS[5]: None, FIELDS[6]: None, FIELDS[7]: None,
+                        FIELDS[8]: None, FIELDS[9]: None, FIELDS[10]: None, FIELDS[0]: user[FIELDS[0]],
+                        FIELDS[1]: user[FIELDS[1]], FIELDS[2]: user[FIELDS[2]],
+                        FIELDS[3]: user[FIELDS[3]], FIELDS[4]: user[FIELDS[4]],
+                        FIELDS[5]: user[FIELDS[5]], FIELDS[6]: user[FIELDS[6]],
+                        FIELDS[7]: user[FIELDS[7]], FIELDS[8]: user[FIELDS[8]],
+                        FIELDS[9]: user[FIELDS[9]], FIELDS[10]: user[FIELDS[10]]}
 
         user_list.append(user_details)
 
@@ -52,39 +84,45 @@ def read_from_json(filename):
 
 
 def submit(entries):
+    """
+    This function is executed when the user fills in all the inforamtion and clicks submit.
+    It takes all the entered information an writes it to a .json file (users.json)
+    :param entries: an array of entries that contain the entered information
+    :return: void
+    """
     cont = True
     for i in range(len((entries))):
-        if entries[fields[i]].get() == "":
+        if entries[FIELDS[i]].get() == "":
             cont = False
             txt_result.config(
                 text="Please complete the required field!", fg="red")
-    if entries[fields[3]].get() != entries[fields[4]].get():
+    if entries[FIELDS[3]].get() != entries[FIELDS[4]].get():
         cont = False
         txt_result.config(text="Passwords do not match!", fg="red")
 
     if cont:
         dict = {}
         for i in range(len(entries)):
-            dict[fields[i]] = entries[fields[i]].get()
+            dict[FIELDS[i]] = entries[FIELDS[i]].get()
 
         users_list = []
-        if os.path.exists(users_file_name) and os.stat(users_file_name).st_size != 0:
-            users_list = read_from_json(users_file_name)
+        if os.path.exists(USERS_FILENAME) and os.stat(USERS_FILENAME).st_size != 0:
+            users_list = read_from_json(USERS_FILENAME)
             users_list.append(dict)
         else:
             users_list.append(dict)
 
         # print(dict)
 
-        filename = str(entries[fields[0]].get()).replace(" ", "") + ".json"
+        filename = str(entries[FIELDS[0]].get()).replace(" ", "") + ".json"
         filename = "/Users/nadeem/Documents/Khwarizm/Alpine/alpine-install/records/" + filename
-        filename = users_file_name
+        filename = USERS_FILENAME
         # with open(filename,
         #           "w") as write_file:  # change "w" to "a" if you want to append instead of overwrite
         #     json.dump(dict, write_file, indent=4)
 
         jsonString = json.dumps(users_list, indent=4)
-        jsonFile = open(users_file_name, "w")
+        jsonFile = open(USERS_FILENAME, "w")
         jsonFile.write(jsonString)
         jsonFile.close()
 
@@ -119,16 +157,28 @@ def submit(entries):
     cmd = 'rc-update add lxdm'
     os.system(cmd)
     """
+    return
 
 
 def clear(entries):
-    for i in range(len(fields)):
-        entries[fields[i]].delete(0, 'end')
+    """
+    This function is executed when the users clicks the "clear" button.
+    It resets the entire form
+    :param entries: an array of entries to clear
+    :return: void
+    """
+    for i in range(len(FIELDS)):
+        entries[FIELDS[i]].delete(0, 'end')
     txt_result.config(text="Cleared form!", fg="green")
     return
 
 
 def quit():
+    """
+    This function is executed when the users clicks the "quit" button.
+    It quits the entire application
+    :return: void
+    """
     result = messagebox.askquestion(
         'Khwarizm Consulting', 'Are you sure you want to exit?', icon="warning")
     if result == 'yes':
@@ -137,12 +187,22 @@ def quit():
 
 
 def text_alert():
+    """
+    This function creates the label where we will later add some alerts to the user like
+    "please complete the required field" or "Submitted data successfully"
+    :return: void
+    """
     global txt_result
     txt_result = Label(root)
     txt_result.pack()
+    return
 
 
 def create_buttons():
+    """
+    This function creates 3 buttons (submit, clear, and quit) and associates them with the appropriate methods
+    :return: void
+    """
     top = Frame(root)
     top.pack(side=TOP)
     b = Button(root, text="Submit", command=(lambda e=ents: submit(e)))
@@ -151,16 +211,22 @@ def create_buttons():
     b.pack(in_=top, side=LEFT)
     c.pack(in_=top, side=LEFT)
     d.pack(in_=top, side=LEFT)
+    return
 
 
 def initialise_window():
+    """
+    This function initialises the Tkinter GUI window
+    :return: root Tkinter window
+    """
     global root, ents
     root = Tk()
-    ents = make_form(root, fields)
+    ents = make_form(root, FIELDS)
     root.geometry("800x550")
     root.title("Khwarizm Consulting")
     root.bind('<Return>', (lambda event, e=ents: fetch(e)))
     return root
 
 
+# calling function to initialise the GUI window
 root = initialise_window()
