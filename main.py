f56418850c350352f609fe5d449bb5fafc6a031e
nabd0018 <nabd0018@student.monash.edu>
2022-04-04 17:09:03 +0200

    commented main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from functions import text_alert, create_buttons, initialise_window, root
+"""
+ALPINE INSTALL SCREEN Application
+- Records all submitted user records as a an array of json objects in the file "users.json"
+
+Created by: Nadeem Abdelkader on 3/4/2022
+Last updated by Nadeem Abdelkader on 4/4/2022
+
+GUI framework = Tkinter
+"""
+
+# !/usr/bin/env python3
+
+# importing the helper functions from functions.py
+from functions import text_alert, create_buttons, root
 
 """
 On Startup
@@ -30,6 +43,9 @@ os.system(cmd)
 """
 
 if __name__ == '__main__':
+    """
+    calling the helper functions from functions.py to start and run the application
+    """
     text_alert()
     create_buttons()
     root.mainloop()
